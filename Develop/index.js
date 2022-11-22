0f61f046e0f26286edb997408fe6cac4a5ace74d
92castro <celin92castro@gmail.com>
2022-11-21 23:05:34 -0700

    created function to generate the readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,97 +1,95 @@
-// TODO: Include packages needed for this application
+// function to initialize app
 
-const inquirer = require("inquirer");
-const fs = require("fs");
+function init() {
+  // packages needed for this application
 
-// TODO: Create an array of questions for user input
+  const inquirer = require("inquirer");
+  const fs = require("fs");
+  const generate = require("./utils/generateMarkdown.js");
+  const path = require("path");
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "Provide a title for your project:",
-      name: "projectTitle",
-    },
-    {
-      type: "input",
-      message: "Provide a brief description of your project:",
-      name: "description",
-    },
-    {
-      type: "list",
-      message: "Would you like to add a Table of Contents?",
-      choices: ["Yes", "No"],
-      name: "tableOfContents",
-    },
-    {
-      type: "input",
-      message: "Provide the steps required to install your project (if any):",
-      name: "installation",
-    },
-    {
-      type: "input",
-      message: "Provide instructions and examples for use:",
-      name: "usage",
-    },
-    {
-      type: "input",
-      message: "List your collaborators (if any):",
-      name: "credits",
-    },
-    {
-      type: "list",
-      message: "Select the appropriate license for your project:",
-      choices: [
-        "Apache 2.0",
-        "Artistic 2.0",
-        "ISC",
-        "MIT",
-        "Mozilla Public 2.0",
-        "The Unlicense",
-      ],
-      name: "license",
-    },
-    {
-      type: "list",
-      message: "Select the corresponding badge for your license:",
-      choices: [
-        "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
-        "[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)",
-        "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
-        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
-        "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
-        "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
-      ],
-      name: "badge",
-    },
-    {
-      type: "input",
-      message: "Provide any tests for your application (if any)",
-      name: "tests",
-    },
-    {
-      type: "input",
-      message: "What is your GitHub?",
-      name: "gitHub",
-    },
-    {
-      type: "input",
-      message: "What is your email?",
-      name: "email",
-    },
-  ])
+  // array of questions for user input
 
-  .then((response) => {
-    fs.writeFile("readme.md", JSON.stringify(response), (err) => {
-      err ? console.error(err) : console.log("success");
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Provide a title for your project:",
+        name: "projectTitle",
+      },
+      {
+        type: "input",
+        message: "Provide a brief description of your project:",
+        name: "description",
+      },
+      {
+        type: "input",
+        message: "Provide the steps required to install your project (if any):",
+        name: "installation",
+      },
+      {
+        type: "input",
+        message: "Provide instructions and examples for use:",
+        name: "usage",
+      },
+      {
+        type: "input",
+        message: "List your collaborators (if any):",
+        name: "credits",
+      },
+      {
+        type: "list",
+        message: "Select the appropriate license for your project:",
+        choices: [
+          "Apache 2.0 ",
+          "Artistic 2.0",
+          "ISC",
+          "MIT",
+          "Mozilla Public 2.0",
+          "The Unlicense",
+        ],
+        name: "license",
+      },
+      {
+        type: "list",
+        message: "Select the corresponding badge for your license:",
+        choices: [
+          "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+          "[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)",
+          "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
+          "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+          "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+          "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
+        ],
+        name: "badge",
+      },
+      {
+        type: "input",
+        message: "Provide any tests for your application (if any)",
+        name: "tests",
+      },
+      {
+        type: "input",
+        message: "What is your GitHub Url?",
+        name: "gitHub",
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+      },
+    ])
+
+    // TODO: Create a function to write README file
+
+    .then((response) => {
+      fs.writeFileSync(
+        path.join(process.cwd(), "ReadMedemo.md"),
+        generate(response)
+      );
+      console.log("Readme created");
     });
-  });
+}
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
+// Function call to initialize app
+init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,48 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectTitle}
 
-`;
+  ${data.badge}
+
+  ## Table of Contents:
+  1. [Description](#description) 
+  2. [Installation](#installation)
+  3. [Usage](#usage)  
+  4. [Credits](#credits)
+  5. [Tests](#tests)
+  6. [License](#license)
+  7. [Questions](#questions)
+
+## Description
+
+${data.description} 
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Credits
+
+${data.credits}
+
+## Tests
+
+${data.tests}
+
+## Questions
+How to contact me with questions or collaborations:
+
+${data.gitHub}
+
+${data.email}
+
+## License
+
+${data.license}`;
 }
 
 module.exports = generateMarkdown;
